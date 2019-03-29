b6f5b74d979d00f3616a154f84431307432b7d0c
Ken <>
2019-03-29 17:41:12 -0400

    Update stall sensitivity, motor currents, and accelerations

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1452,7 +1452,7 @@
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT     650  // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT     800  // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_MICROSTEPS   16  // 0..256
     #define X_RSENSE     0.11
   #endif
@@ -1464,7 +1464,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT     650
+    #define Y_CURRENT     800
     #define Y_MICROSTEPS   16
     #define Y_RSENSE     0.11
   #endif
@@ -1476,7 +1476,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT     650
+    #define Z_CURRENT     800
     #define Z_MICROSTEPS   16
     #define Z_RSENSE     0.11
   #endif
@@ -1494,7 +1494,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT    350
+    #define E0_CURRENT    200
     #define E0_MICROSTEPS  16
     #define E0_RSENSE    0.11
   #endif
@@ -1657,8 +1657,8 @@
   //#define SENSORLESS_PROBING // TMC2130 only
 
   #if EITHER(SENSORLESS_HOMING, SENSORLESS_PROBING)
-    #define X_STALL_SENSITIVITY  4
-    #define Y_STALL_SENSITIVITY  7
+    #define X_STALL_SENSITIVITY  8
+    #define Y_STALL_SENSITIVITY  10
     //#define Z_STALL_SENSITIVITY  8
   #endif
 
