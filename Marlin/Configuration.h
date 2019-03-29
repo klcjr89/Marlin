78cb4a27bccb892617d159b20bf541743b67378c
Ken <>
2019-03-29 16:10:47 -0400

    Update Y size and home offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1001,11 +1001,11 @@
 
 // The size of the print bed
 #define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define Y_BED_SIZE 215
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -15
-#define Y_MIN_POS -26
+#define Y_MIN_POS -25
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
