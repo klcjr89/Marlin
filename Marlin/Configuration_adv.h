3ad8a3dc0ae3bb41a98b310bd6a308248edf4484
Ken <>
2019-03-30 02:25:01 -0400

    Update sense resistor values

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1454,7 +1454,7 @@
   #if AXIS_IS_TMC(X)
     #define X_CURRENT     800  // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_MICROSTEPS   16  // 0..256
-    #define X_RSENSE     0.11
+    #define X_RSENSE     0.075
   #endif
 
   #if AXIS_IS_TMC(X2)
@@ -1466,7 +1466,7 @@
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT     800
     #define Y_MICROSTEPS   16
-    #define Y_RSENSE     0.11
+    #define Y_RSENSE     0.075
   #endif
 
   #if AXIS_IS_TMC(Y2)
@@ -1478,7 +1478,7 @@
   #if AXIS_IS_TMC(Z)
     #define Z_CURRENT     800
     #define Z_MICROSTEPS   16
-    #define Z_RSENSE     0.11
+    #define Z_RSENSE     0.075
   #endif
 
   #if AXIS_IS_TMC(Z2)
@@ -1496,7 +1496,7 @@
   #if AXIS_IS_TMC(E0)
     #define E0_CURRENT    200
     #define E0_MICROSTEPS   8
-    #define E0_RSENSE    0.11
+    #define E0_RSENSE    0.075
   #endif
 
   #if AXIS_IS_TMC(E1)
