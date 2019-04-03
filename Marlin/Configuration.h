a45714016879f9a08e631932d68bccdfebf55b35
Ken <>
2019-04-03 12:10:01 -0400

    Update homing sensitvity, Y bed size and home position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1001,11 +1001,11 @@
 
 // The size of the print bed
 #define X_BED_SIZE 220
-#define Y_BED_SIZE 215
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -6
-#define Y_MIN_POS -25
+#define Y_MIN_POS -20
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
