6ed46457ca09af2c1385772ae83a32a862c7af1d
Ken <>
2019-07-08 20:08:58 -0400

    Increase max bed temp for testing Staymag™

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -443,7 +443,7 @@
 #define HEATER_3_MAXTEMP 275
 #define HEATER_4_MAXTEMP 275
 #define HEATER_5_MAXTEMP 275
-#define BED_MAXTEMP      150
+#define BED_MAXTEMP      200
 
 //===========================================================================
 //============================= PID Settings ================================
