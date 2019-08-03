d01f75bd6ff7f8f54fef7622becb8bc786770282
Ken <>
2019-08-02 20:12:48 -0400

    Update X home offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1052,7 +1052,7 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -4
+#define X_MIN_POS -7
 #define Y_MIN_POS -24
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
